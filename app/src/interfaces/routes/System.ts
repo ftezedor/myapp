efcdb503bd4b4520658aabdea88805ce9b60bf21
Fabio Tezedor <fabio@tezedor.com.br>
2025-04-04 04:22:19 -0300

    enhancement

--- app/src/interfaces/routes/System.ts
+++ app/src/interfaces/routes/System.ts
@@ -1,14 +1,11 @@
-import UserController from "@src/infrastructure/controllers/UserController";
 import { BaseRequest, BaseResponse, BaseServer } from "@src/infrastructure/servers/base";
-import { Authenticator } from "@src/shared/utils/Authenticator";
-import { stat } from "fs";
-import { version } from "os";
+import * as os from 'os';
 
 export default class AuthRoutes {
     public static async define(server: BaseServer) {
         
         server.setRoute('GET', '/', async (req: BaseRequest, res: BaseResponse) => {
-            res.send({ version: '1.23.10', build: '20250402' });
+            res.json({ version: '1.23.10', build: '20250402', hostname: os.hostname });
         });
 
         server.setRoute('GET', '/healthz', async (req: BaseRequest, res: BaseResponse) => {
